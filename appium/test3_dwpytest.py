5ccf0f542080b618ef96d1ba3a9c4325e98e5c0c
莉光茹 <luckylhia@163.com>
2023-09-26 19:02:20 +0800

    update---20230926

--- appium/test3_dwpytest.py
+++ appium/test3_dwpytest.py
@@ -1,9 +1,13 @@
 # 时间：2023/9/18 16:51
 # 人员: 莉光哈哈哈
+import time
+
 import pytest
 from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestDW:
@@ -27,11 +31,16 @@ class TestDW:
         self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/home_search").click()
         self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴")
         self.driver.find_element(by=By.XPATH, value="//android.widget.TextView[@text='股票']").click()
-        self.driver.find_element(by=By.XPATH,
-                                 value="//android.widget.ListView[@resource-id='com.xueqiu.android:id/lv_list_view']/android.widget.RelativeLayout[1]").click()
+
+        # locator = (By.XPATH, "//android.widget.ListView[@resource-id='com.xueqiu.android:id/lv_list_view']/android.widget.RelativeLayout[1]")
+        # WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(locator))
+        #
+        # self.driver.find_element(by=By.XPATH, value="locator").click()
+        self.driver.find_element(by=By.XPATH, value="//android.widget.ListView[@resource-id='com.xueqiu.android:id/lv_list_view']/android.widget.RelativeLayout[1]").click()
 
     def test_attr(self):
-        '''
+
+        """
         打开【雪球】应用首页
         定位首页的搜索框
         判断搜索框是否可用，并查看搜索框name属性值
@@ -40,7 +49,7 @@ class TestDW:
         判断【阿里巴巴】是否可见
         如果可见，打印“搜索成功”点击，如果不可见，打印“搜索失败”
         :return:
-        '''
+        """
         element = self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/home_search")
         search_enabled = element.is_enabled()
         print(element.text)
@@ -71,6 +80,25 @@ class TestDW:
 
     # def test_touchAction_1(self):
     #     print("解锁手势密码")
+
+    def test_myInfo(self):
+        """
+        1、点击登录，跳转个人登陆页面
+        2、点击账号密码登录
+        3、输入手机号、密码
+        4、勾选协议
+        5、点击登录
+        """
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/tv_login").click()
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/login_outside").click()
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/login_account").send_keys("15036205307")
+        time.sleep(3)
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/login_password").send_keys("zhangyaru1024")
+        time.sleep(3)
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/button_next").click()
+        time.sleep(3)
+        self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/tv_agree").click()
+        time.sleep(3)
 
 
 if __name__ == '__main__':
